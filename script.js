b92f929d6877e5de5649e9db22fcc870641ab103
Alison Lubar <aslubar@gmail.com>
2020-10-06 21:41:17 -0400

    prepended previous searches

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ $(document).ready(function () {
 
     console.log(city);
 
+    //prepending to div here
+    $("#previousSearchList").prepend('<button type="button" class="btn btn-primary btn-md btn-block">' + city + '</button>')
+
+    // Need to add these classes: type="button" class="btn btn - primary btn - md btn - block"
+    // $("button").addClass("btn btn-primary btn-md btn-block")
+
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -29,10 +35,10 @@ $(document).ready(function () {
 
       var d = new Date();
       var todaysDate =
-        d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+        (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
 
       var tempF = Math.floor((response.main.temp - 273.15) * 1.8 + 32);
-      $("#cityNameText").html(response.Name + todaysDate);
+      $("#cityNameText").html(response.name + " " + todaysDate);
       $(".currentHumidity").html("Current Humidity: " + response.main.humidity);
       $(".currentTemp").html("Current Temperature: " + tempF);
       $(".currentWind").html(
@@ -46,3 +52,4 @@ $(document).ready(function () {
 //use lat and long from this API to get the ONE CALL API, then use the same .html appending to enter that info-- just a LOT of it-- you need th
 
 //LUXON for the date (or moment-- that's fine, too)
+//img.attr("src", "url...")
